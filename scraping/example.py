bd227612b96654b80837c42052d484e8a8363e2c
seongmin-97 <seongmin.tech@gmail.com>
2021-01-17 02:12:07 +0900

    add naverMovieData

--- scraping/example.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import functools as ft
-import numpy as np
-import pandas as pd
-from time import sleep
-import re
-from pprint import pprint
-import time
